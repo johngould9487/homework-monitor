7a3c9f06b87ca1505a672084e0dff6c36d111d39
John Gould <johngould9487@gmail.com>
2018-09-01 11:08:04 +0100

    general tidy up and flow

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,12 +1,14 @@
 class ChildrenController < ApplicationController
   def index
-      @children = current_user.students
+    @children = policy_scope(User).all.select do |user|
+      user.guardians.include?(current_user)
+    end
+    # @children = policy_scope(current_user.students)
+    authorize @children.first
   end
 
   def show
-      @child = User.find(params[:id])
+    @child = User.find(params[:id])
+    authorize @child
   end
-    # def show
-    # newsletter placeholder
-    # end
 end
