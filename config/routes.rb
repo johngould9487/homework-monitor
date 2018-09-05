9eb07e52fbce7fe07bfdaabf847cc9de6b0821cb
Syed Abdullah Ali <mtuais95@gmail.com>
2018-09-05 19:10:42 +0100

    welcome message edited

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'children/show'
   # get 'teaching_groups/index' #this seems to be the landing page
   # get 'teaching_groups/show'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
