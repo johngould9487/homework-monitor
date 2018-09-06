8c876947f437b2ad48994c44c1e0f297d4e028da
John Gould <johngould9487@gmail.com>
2018-09-06 15:39:07 +0100

    changed fault in guardian view

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,20 +7,22 @@ class AssignmentsController < ApplicationController
       @teaching_group = TeachingGroup.find(params[:teaching_group_id])
       @assignments = Assignment.where(teaching_group: @teaching_group).where('date_due > ?', Date.today)
       authorize(@assignments.first)
+      @assignments = @assignments.sort_by { |assignment| assignment.date_due }
     elsif current_user.student
       @teaching_group = TeachingGroup.find(params[:teaching_group_id])
       @attempts = Attempt.all.select do |attempt|
         attempt.student == current_user && attempt.assignment.date_due > Date.today && attempt.assignment.teaching_group == @teaching_group
       end
       authorize(@attempts.first)
+      @assignments = @assignments.sort_by { |assignment| assignment.date_due }
     else
       @child = User.find(params[:child_id])
       @attempts = Attempt.all.select do |attempt|
         attempt.student == User.find(params[:child_id]) && attempt.assignment.date_due > Date.today
       end
       authorize(@attempts.first)
+      @attempts = @attempts.sort_by { |attempt| attempt.assignment.date_due }
     end
-    @assignments = @assignments.sort_by { |assignment| assignment.date_due }
   end
 
   def past
