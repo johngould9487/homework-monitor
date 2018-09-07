2cd1094d378b3f9d0c6a7edccef168df1d3e3577
Alex Kelly <alexkelly51@gmail.com>
2018-09-07 12:39:30 +0100

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,7 +81,7 @@ teaching_groups.each_with_index do |teaching_group, teaching_group_index|
   end
 end
 
-indices = [[0,0], [0,1], [0,2], [1,1], [2,2], [3,3], [4,3], [4,4], [5,4], [6,5], [7,5], [8,6], [9,6], [8,7], [9,7], [10,8], [11,9], [12,9]]
+indices = [[0,0], [1,1], [2,2], [3,3], [4,3], [4,4], [5,4], [6,5], [7,5], [8,6], [9,6], [8,7], [9,7], [10,8], [11,9], [12,9]]
 indices.each do |index_pair|
   FamilyConnection.create!(student:students[index_pair[1]], guardian:guardians[index_pair[0]])
 end
@@ -211,7 +211,7 @@ end
     grading_type: "OUT OF TEN",
     maximum_score: 10,
     teaching_group: teaching_groups[1],
-    title: "HTML basics - know your code"
+    title: "HTML basics - know your code!"
   )
 
   Assignment.create!(
